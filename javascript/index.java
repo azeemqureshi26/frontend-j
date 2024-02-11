40a9ca98d0fdc34db50a906e30cfdacbed741f7d
azeem <azeemqureshi7860@gmail.com>
2024-02-12 01:09:11 +0530

    created

---
+++ javascript/index.java
@@ -0,0 +1,10 @@
+class index{
+    public static void main(String[] args) {
+            int[][] arr  = {{23, 33, 44} , {23, 33, 44}};
+            //System.out.println(arr[0][2]);
+            for(int i=0; i<=arr.length; i++){
+                System.out.println(i);
+            }
+
+    }
+}
